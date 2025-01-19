d86bc72b20811190292968873c19cc652e42270a
Renz <140231442+lawrnzzz@users.noreply.github.com.>
2025-01-20 04:18:13 +0800

    chore: setup development environment

---
+++ webpack.common.js
@@ -0,0 +1,34 @@
+const path = require("path");
+const HtmlWebpackPlugin = requre("html-webpack-plugin");
+
+module.exports = {
+  entry: "./src/index.html",
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.html$/i,
+        use: ["html-loader"],
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: "asset/resource",
+      },
+
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
+      },
+    ],
+  },
+  plugins: [new HtmlWebpackPlugin({ template: "./src/template.html" })],
+};
